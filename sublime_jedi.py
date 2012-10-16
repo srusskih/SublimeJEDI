f8377a46ec743eb7fca90fd7ca95164e9ce23560
Samuel Vaiter <samuel.vaiter@gmail.com>
2012-10-16 09:59:03 +0200

    Working start

--- sublime_jedi.py
+++ sublime_jedi.py
@@ -1,6 +1,21 @@
+import os
+
 import sublime
 import sublime_plugin
-import jedi
+import imp
+
+jedi = imp.load_source('jedi', os.getcwd() + '/jedi')
+
+def get_script(v):
+    text = v.substr(sublime.Region(0, v.size()))
+    source_path = v.file_name()
+    if len(v.sel()) == 1:
+        (current_line,current_column) = v.rowcol(v.sel()[0].begin())
+    else:
+        pass
+    return jedi.Script(text, current_line, current_column, source_path)
 
 class SublimeJedi(sublime_plugin.TextCommand):
-    our_jedi = jedi.Script()
+    def run(self, edit):
+        anakin = get_script(self.view)
+        print anakin.complete()
