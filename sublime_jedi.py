05ec31998f973128952318b4f8403d74370d4b25
srusskih <smilemouse2002@ukr.net>
2013-05-07 22:57:37 +0300

    issue #35: function args completion behavior changed

--- sublime_jedi.py
+++ sublime_jedi.py
@@ -57,6 +57,26 @@ def get_settings_param(view, param_name, default=None):
     )
 
 
+def get_function_parameters(callDef):
+    """ (jedi.api_classes.CallDef) -> list of tuple(str, str)
+
+    Return list function paramets, prepared for sublime completion.
+    Tuple contains parameter name and default value
+
+    Parameters list excludes: self, *args and **kwargs parameters
+    """
+    if not callDef:
+        return []
+
+    params = []
+    for param in callDef.params:
+        cleaned_param = param.get_code().strip()
+        if '*' in cleaned_param or cleaned_param == 'self':
+            continue
+        params.append([s.strip() for s in cleaned_param.split('=')])
+    return params
+
+
 class JediEnvMixin(object):
     """ Mixin to install user virtual env for JEDI """
 
@@ -143,81 +163,96 @@ class JediEnvMixin(object):
 
 
 class SublimeMixin(object):
-    """ helpers to integrate sublime """
-
+    """
+    helpers to integrate sublime
+    """
     def is_funcargs_complete_enabled(self, view):
         return get_settings_param(view, 'auto_complete_function_params')
 
     def is_funcargs_all_complete_enabled(self, view):
         return get_settings_param(view, 'auto_complete_function_params') == "all"
 
-    def format(self, complete, insert_funcargs=True, insert_all_funcargs=True):
-        """ Returns a tuple of the string that would be visible in the completion
-            dialogue, and the snippet to insert for the completion
+    def format(self, complete):
+        """ Returns a tuple of the string that would be visible in
+            the completion dialogue and the completion word
 
             :param complete: `jedi.api.Complete` object
             :return: tuple(string, string)
         """
         display, insert = complete.word + '\t' + complete.type, complete.word
-        if not insert_funcargs:
-            if complete.type == 'function':
-                # if its a function add parentheses
-                return display, insert + "(${1})"
-            return display, insert
-
-        if hasattr(complete.definition, 'params'):
-            params = []
-            for index, param in enumerate(complete.definition.params):
-                # Strip all whitespace to make it PEP8 compatible
-                code = [s.strip() for s in param.get_code().strip().split('=')]
-                if 'self' in code or code[0].startswith('*'):
-                    # drop self and *args, **kwargs from method calls
-                    continue
-
-                if len(code) > 1 and insert_all_funcargs:
-                    # param with default value
-                    params.append("%s=${%d:%s}" % (code[0], index + 1, code[1]))
-                elif len(code) == 1:
-                    # required param
-                    params.append("${%d:%s}" % (index + 1, code[0]))
-            insert = "%(fname)s(%(params)s)" % {
-                'fname': insert,
-                'params': ', '.join(params)
-            }
         return display, insert
 
     def funcargs_from_script(self, script):
         """ get completion in case we are in a function call """
         completions = []
-        in_call = script.get_in_function_call()
-        if in_call is not None:
-            for calldef in in_call.params:
-                # Strip all whitespace to make it PEP8 compatible
-                code = calldef.get_code().strip()
-                if '*' in code or code == 'self':
-                    continue
-                code = [s.strip() for s in code.split('=')]
-                if len(code) == 1:
-                    completions.append((code[0], '%s=${1}' % code[0]))
-                else:
-                    completions.append((code[0] + '\t' + code[1],
-                                       '%s=${1:%s}' % (code[0], code[1])))
+        in_call = script.function_definition()
+
+        params = get_function_parameters(in_call)
+        for code in params:
+            if len(code) == 1:
+                completions.append((code[0], '%s=${1}' % code[0]))
+            else:
+                completions.append((code[0] + '\t' + code[1],
+                                   '%s=${1:%s}' % (code[0], code[1])))
         return completions
 
     def completions_from_script(self, script, view):
         """ regular completions """
         completions = script.complete()
-        insert_funcargs = self.is_funcargs_complete_enabled(view)
-        insert_all_funcargs = self.is_funcargs_all_complete_enabled(view)
-        completions = [
-            self.format(complete, insert_funcargs, insert_all_funcargs)
-            for complete in completions
-        ]
+        completions = [self.format(complete) for complete in completions]
         return completions
 
 
-class Autocomplete(JediEnvMixin, SublimeMixin, sublime_plugin.EventListener):
+class SublimeJediParamsAutocomplete(JediEnvMixin, SublimeMixin,
+                                    sublime_plugin.TextCommand):
+    """
+    Function / Class constructor autocompletion command
+    """
+    def run(self, edit, characters='('):
+        regions = [a for a in self.view.sel()]
+        self.view.sel().clear()
 
+        is_enabled = self.is_funcargs_complete_enabled(self.view)
+
+        for region in reversed(regions):
+            position = 0
+            if region.size() > 0:
+                self.view.replace(edit, region, characters)
+                position = region.begin() + len(characters)
+            else:
+                self.view.insert(edit, region.end(), characters + ')')
+                position = region.end() + len(characters)
+
+            self.view.sel().add(sublime.Region(position, position))
+
+            # if function arguemtns completion enabled
+            # return list of function args
+            if is_enabled:
+                with self.env:
+                    self.get_complete(position)
+
+    def get_complete(self, position):
+        complete_all = self.is_funcargs_all_complete_enabled(self.view)
+        script = get_script(self.view, position)
+        parameters = get_function_parameters(script.function_definition())
+
+        completions = []
+        for index, parameter in enumerate(parameters):
+            name = parameter[0]
+            if len(parameter) > 1 and complete_all:
+                value = parameter[1]
+                completions.append('%s=${%d:%s}' % (name, index + 1, value))
+            elif len(parameter) == 1:
+                completions.append('%s=${%d}' % (name, index + 1))
+
+        template = ", ".join(completions)
+        self.view.run_command('insert_snippet', {"contents": template})
+
+
+class Autocomplete(JediEnvMixin, SublimeMixin, sublime_plugin.EventListener):
+    """
+    Sublime Text autocompletion integration
+    """
     def on_query_completions(self, view, prefix, locations):
         """ Sublime autocomplete event handler
 
